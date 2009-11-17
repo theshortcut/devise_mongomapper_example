08477ad6b18ec24540b722bcbb907f290c44ee62
Clayton Ferris <cferris@gmail.com>
2009-11-17 16:23:33 -0700

    functional test for requiring login on user/edit. getting a no method utc for Date issue after sign_in @user. look at and maybe fix issue with devise

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -12,4 +12,23 @@ class UsersControllerTest < ActionController::TestCase
     should_render_template :new
     should_not_set_the_flash
   end
+
+  context "on GET to :edit while not signed in" do
+    setup do
+      get :edit
+    end
+
+    should_redirect_to("default") { new_user_session_url(:unauthenticated => true) }
+  end
+
+  context "on GET to :edit while signed in" do
+    setup do
+      @user = Factory(:user)
+      sign_in @user
+      get :edit
+    end
+
+    should_respond_with :success
+    should_render_template :edit
+  end
 end
