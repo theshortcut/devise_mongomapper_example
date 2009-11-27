f2760d5746b0afd13be0a8cb367ce0e458b67045
Clayton Ferris <cferris@gmail.com>
2009-11-26 22:32:39 -0700

    ignoring compiled css files. bumping versions of gems and rails (yay for patch free rails on ruby 1.9)

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -15,11 +15,11 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem 'warden', :version => '>=0.6.1', :source => 'http://gemcutter.org'
-  config.gem 'devise', :version => '>=0.5.3', :source => 'http://gemcutter.org'
-  config.gem 'mongo_mapper', :version => '>=0.6.1', :source => 'http://gemcutter.org'
+  config.gem 'warden', :version => '>=0.6.4', :source => 'http://gemcutter.org'
+  config.gem 'devise', :version => '>=0.6.2', :source => 'http://gemcutter.org'
+  config.gem 'mongo_mapper', :version => '>=0.6.4', :source => 'http://gemcutter.org'
   config.gem 'compass', :version => '>=0.8.17', :source => 'http://gemcutter.org'
-  config.gem 'haml', :version => '>=2.2.0'
+  config.gem 'haml', :version => '>=2.2.14'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
