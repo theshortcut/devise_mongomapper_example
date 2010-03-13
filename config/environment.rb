177f291a44a0e68cf702b189f089c6fcd33c18bd
Clayton Ferris <cferris@gmail.com>
2010-03-13 10:26:59 -0600

    updated mongo_mapper to 0.7.1

--- config/environment.rb
+++ config/environment.rb
@@ -16,9 +16,9 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   config.gem 'devise', :version => '=1.0.4'
-  config.gem 'mongo_mapper', :version => '=0.7.0'
+  config.gem 'mongo_mapper', :version => '=0.7.1'
   config.gem 'compass', :version => '=0.8.17'
-  config.gem 'haml', :version => '=2.2.20'
+  config.gem 'haml', :version => '=2.2.21'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
